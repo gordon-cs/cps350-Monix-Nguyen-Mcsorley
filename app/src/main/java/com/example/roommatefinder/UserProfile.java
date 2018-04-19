38759a4e6386206b4e871934d615d4c29d55f977
sam-nguyen7 <sam.nguyen@gordon.edu>
2018-04-19 16:25:28 -0400

    retrieve user data for profile page

--- app/src/main/java/com/example/roommatefinder/UserProfile.java
+++ app/src/main/java/com/example/roommatefinder/UserProfile.java
@@ -6,12 +6,50 @@ public class UserProfile {
     public String userClass;
     public String userGender;
 
-    //constructor
+    //constructor, need this because of setter and getter
+    public UserProfile(){
+
+    }
     public UserProfile(String userName, String userEmail, String userClass, String userGender)
     {
         this.userName = userName;
         this.userEmail = userEmail;
         this.userClass = userClass;
+        this.userGender = userGender;
+    }
+
+    //5th step: create getter and setter
+    //
+    //
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserClass() {
+        return userClass;
+    }
+
+    public void setUserClass(String userClass) {
+        this.userClass = userClass;
+    }
+
+    public String getUserGender() {
+        return userGender;
+    }
+
+    public void setUserGender(String userGender) {
         this.userGender = userGender;
     }
 }
